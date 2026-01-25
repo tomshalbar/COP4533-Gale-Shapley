f9642d27a2f4f830f67dc41734362206561b1a11
Tom Sha-bar <tomshalbar@gmail.com>
2026-01-25 18:24:57 -0500

    starting gale shapley implementation

--- src/gale_shapley.py
+++ src/gale_shapley.py
@@ -9,9 +9,41 @@ class GaleShapley:
       
     '''
 
-    def find_matches(self, preference_list1 : dict[int, list[int]], preference_list2 : dict[int, list[int]]) -> list[tuple[int, int]]:
-        free_proposer = [key for key in preference_list1.keys()]
-        # while 
+    def find_matches(self, proposers_preferences : dict[int, list[int]], receivers_preferences : dict[int, list[int]]) -> list[tuple[int, int]]:
+        matches = {key : -1 for key in proposers_preferences.keys()}
+
+        # [NOTE] I would've used sets, but want replicability for east debugging. 
+        free_proposers = list(proposers_preferences.keys())
+        tested_matches = {key : 0 for key in proposers_preferences.keys()}
+
+        while free_proposers:
+            proposer = free_proposers[0]
+
+            if tested_matches[proposer] == len(proposers_preferences[proposer]):
+                free_proposers.remove(proposer)
+                continue
+
+            receiver = proposers_preferences[proposer][tested_matches[proposer]]
+            tested_matches[proposer] += 1
+
+            if receiver not in matches.values():
+                matches[proposer] = receiver
+                free_proposers.remove(proposer)
+                continue 
+            cur_proposer = next(prop for prop, rec in matches.items() if rec == receiver)
+            if receivers_preferences[receiver].index(proposer) > receivers_preferences[receiver].index(cur_proposer):
+                matches[cur_proposer] = -1
+                matches[proposer] = receiver
+                free_proposers.remove(proposer)
+                free_proposers.append(cur_proposer)
+        
+        return matches 
+
+                
+
+
+
+
         
 
     
