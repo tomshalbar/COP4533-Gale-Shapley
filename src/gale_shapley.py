0f352827a7acd02ab2a700cad1e85eb91f2d34b4
Tom Sha-bar <tomshalbar@gmail.com>
2026-01-27 13:46:02 -0500

    Fix G-S logic, black, add doctest

--- src/gale_shapley.py
+++ src/gale_shapley.py
@@ -1,20 +1,26 @@
-
-
 class GaleShapley:
-    '''Implementation of the Gale-Shapley algorithem
-     
-    takes it 2 preference list as a 2D array of ints. First one is the 
-    proposing side. 
+    """Implementation of the Gale-Shapley algorithem
 
-      
-    '''
+    takes it 2 preference list as a 2D array of ints. First one is the
+    proposing side.
+    """
 
-    def find_matches(self, proposers_preferences : dict[int, list[int]], receivers_preferences : dict[int, list[int]]) -> list[tuple[int, int]]:
-        matches = {key : -1 for key in proposers_preferences.keys()}
+    def find_matches(
+        self,
+        proposers_preferences: dict[int, list[int]],
+        receivers_preferences: dict[int, list[int]],
+    ) -> list[tuple[int, int]]:
+        """
+        >>> gs = GaleShapley()
+        >>> gs.find_matches({1: [1, 2, 3], 2: [2, 1, 3], 3: [2, 3, 1]}, {1: [1, 2, 3], 2: [2, 1, 3], 3: [2, 3, 1]})
+        {1: 1, 2: 2, 3: 3}
+        """
 
-        # [NOTE] I would've used sets, but want replicability for east debugging. 
+        matches = {key: -1 for key in proposers_preferences.keys()}
+
+        # [NOTE] I would've used sets, but want replicability for east debugging.
         free_proposers = list(proposers_preferences.keys())
-        tested_matches = {key : 0 for key in proposers_preferences.keys()}
+        tested_matches = {key: 0 for key in proposers_preferences.keys()}
 
         while free_proposers:
             proposer = free_proposers[0]
@@ -29,26 +35,36 @@ class GaleShapley:
             if receiver not in matches.values():
                 matches[proposer] = receiver
                 free_proposers.remove(proposer)
-                continue 
-            cur_proposer = next(prop for prop, rec in matches.items() if rec == receiver)
-            if receivers_preferences[receiver].index(proposer) > receivers_preferences[receiver].index(cur_proposer):
+                continue
+            cur_proposer = next(
+                prop for prop, rec in matches.items() if rec == receiver
+            )
+            if receivers_preferences[receiver].index(proposer) < receivers_preferences[
+                receiver
+            ].index(cur_proposer):
                 matches[cur_proposer] = -1
                 matches[proposer] = receiver
                 free_proposers.remove(proposer)
                 free_proposers.append(cur_proposer)
-        
-        return matches 
 
-                
+        return matches
 
 
+import doctest
 
+# doctest.testmod()
 
-        
-
-    
-
-p1 = {1: [1, 2, 3], 2: [2, 1, 3], 3: [2, 3, 1],}
-p2 = {1: [1, 2, 3], 2: [2, 1, 3], 3: [2, 3, 1],}
+p1 = {
+    1: [1, 2, 3],
+    2: [2, 1, 3],
+    3: [2, 3, 1],
+}
+p2 = {
+    1: [1, 2, 3],
+    2: [2, 1, 3],
+    3: [2, 3, 1],
+}
 gs = GaleShapley()
-gs.find_matches(p1, p2)
+
+m = gs.find_matches(p1, p2)
+print(m)
